6a60b8939ad4daf15cb86e36f39e411a5cbf65e5
Ricardo Castro <mcc.ricardo@gmail.com>
2025-10-02 21:47:26 +0100

    feat: Add readiness, liveress and startup endpoints

--- app/main.go
+++ app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -31,6 +33,33 @@ func main() {
 			isPrime(i)
 		}
 		return c.SendString("Done with heavy lifting!\n")
+	})
+
+	app.Get("/ready", func(c *fiber.Ctx) error {
+		return c.SendString("Ready")
+	})
+
+	app.Get("/live", func(c *fiber.Ctx) error {
+		return c.SendString("Live")
+	})
+
+	app.Get("/start", func(c *fiber.Ctx) error {
+		fmt.Println("Starting up...")
+		time.Sleep(15 * time.Second)
+		fmt.Println("Startup complete!")
+		return c.SendString("Started")
+	})
+
+	app.Get("/ready_fail", func(c *fiber.Ctx) error {
+		return c.Status(500).SendString("Ready probe failed")
+	})
+
+	app.Get("/live_fail", func(c *fiber.Ctx) error {
+		return c.Status(500).SendString("Live probe failed")
+	})
+
+	app.Get("/start_fail", func(c *fiber.Ctx) error {
+		return c.Status(500).SendString("Start probe failed")
 	})
 
 	app.Listen(":3000")
